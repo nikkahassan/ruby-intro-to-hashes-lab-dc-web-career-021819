793cc0e72b2f77e4ae88e15220d7702772beaca1
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-08 16:51:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,17 +15,16 @@ def monopoly_with_second_tier
 	monopoly = {:railroads => 
 	{
 	  :pieces =>4
-	}}
+	  :names => {},
+    :rent_in_dollars => {}
+	}
+	  }
 
 end
 
 def monopoly_with_third_tier
-monopoly = {:railroads => 
-	{
-	  pieces => 4,
-    names => {},
-    rent_in_dollars => {}
-	}}
+
+	  
 end
 
 def monopoly_with_fourth_tier
