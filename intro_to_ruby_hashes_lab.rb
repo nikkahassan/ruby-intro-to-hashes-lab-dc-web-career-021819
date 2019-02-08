227b9ba2abc3f5296723902a0336be8580dde81e
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-08 16:57:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
 	                   :pieces => 4,
 	                   :names => {},
 	                   :rent_in_dollars => {:one_piece_owned => 25,
-	                                        :two_pieces_ownded => 50,
+	                                        :two_pieces_owned => 50,
 	                                        :three_pieces_ownded => 100,
 	                                        :four_pieces_ownded => 200
 	                   }
